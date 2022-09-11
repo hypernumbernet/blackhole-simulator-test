be7365ff90ae3c34fd043c14459c18f371dbdc8f
Tomohito Inoue <hypernumbernet@users.noreply.github.com>
2022-09-11 15:35:08 +0900

    Updated SpacetimeTest

--- spacetimetest.cpp
+++ spacetimetest.cpp
@@ -19,16 +19,28 @@ void SpacetimeTest::test_member()
     QVERIFY(st0.conjugated() == Spacetime(2., -3., -4., -5.));
     QVERIFY(fuzzyCompare(st0.abs(), complex(0., 6.782329983125268)));
 
-    Vector3 direction(1., 2., 3.);
-    direction.normalize();
-    QVERIFY(direction.norm() == 1.);
+    Vector3 direction1(1., 2., 3.);
+    direction1.normalize();
+    QVERIFY(direction1.norm() == 1.);
 
     // v = c tanh( a )
     // a = atanh( v / c )
     double v0 = 1e+7;
     double rapidity0 = atanh(v0 / SPEED_OF_LIGHT);
-    Spacetime g0 = Spacetime::exp(0.5 * rapidity0, direction);
+    Spacetime g0 = Spacetime::exp(0.5 * rapidity0, direction1);
     Spacetime ts0(SPEED_OF_LIGHT, 0., 0., 0.);
     ts0.lorentzTransformation(g0);
     qDebug() << "ts0:" << ts0;
+
+    const double v1 = 0.9;
+    Vector3 direction2(1., 0., 0.);
+    double rapidity1 = atanh(v1);
+    Spacetime g1 = Spacetime::exp(0.5 * rapidity1, direction2);
+    qDebug() << "g1:" << g1;
+    Spacetime ts1(SPEED_OF_LIGHT, 0., 0., 0.);
+    ts1.lorentzTransformation(g1);
+    qDebug() << "ts1:" << ts1;
+    qDebug() << "SPEED_OF_LIGHT * " << v1 << ":" << SPEED_OF_LIGHT * v1;
+    qDebug() << "speed:" << - ts1.x() * (SPEED_OF_LIGHT / ts1.w());
+    QVERIFY(fuzzyCompare(SPEED_OF_LIGHT * v1, - ts1.x() * (SPEED_OF_LIGHT / ts1.w())));
 }
